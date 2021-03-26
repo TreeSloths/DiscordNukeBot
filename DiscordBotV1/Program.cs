796fc3ab24435999a29cd3ad26091e886d29cabf
Adam Alzuhairi <adam_alzu@hotmail.com>
2021-03-26 20:57:27 +0100

    Update Program.cs

--- DiscordBotV1/Program.cs
+++ DiscordBotV1/Program.cs
@@ -13,29 +13,6 @@ namespace DiscordBotV1
 {
     class Program
     {
-        /*[Command("kick"), Alias("Kick"), Summary("Kicks a user from the server")]
-        public async Task Kick(SocketGuildUser userAccount, string reason)
-        {
-            var user = Context.User as SocketGuildUser;
-            var role = (user as IGuildUser).Guild.Roles.FirstOrDefault(x => x.Name == "Admin");
-            if (!userAccount.Roles.Contains(role))
-            {
-                if (user.GuildPermissions.KickMembers)
-                {
-                    await userAccount.KickAsync(reason);
-                    await Context.Channel.SendMessageAsync($"The user `{userAccount}` has been kicked, for {reason}");
-                }
-                else
-                {
-                    await Context.Channel.SendMessageAsync("No permissions for kicking a user.");
-                }
-            }
-            else
-            {
-                await Context.Channel.SendMessageAsync("This User can't be kicked, because the user has a admin role.");
-            }
-        }*/
-
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
