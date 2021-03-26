00cf8f524a3acc5c63165aa09ccc6b24557dee4a
Adam Alzuhairi <adam_alzu@hotmail.com>
2021-03-26 20:54:02 +0100

    Initial commit

---
+++ DiscordBotV1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Net;
+using Discord.WebSocket;
+using Discord.API;
+using Discord.Rest;
+using MemberInfo = System.Reflection.MemberInfo;
+
+namespace DiscordBotV1
+{
+    class Program
+    {
+        /*[Command("kick"), Alias("Kick"), Summary("Kicks a user from the server")]
+        public async Task Kick(SocketGuildUser userAccount, string reason)
+        {
+            var user = Context.User as SocketGuildUser;
+            var role = (user as IGuildUser).Guild.Roles.FirstOrDefault(x => x.Name == "Admin");
+            if (!userAccount.Roles.Contains(role))
+            {
+                if (user.GuildPermissions.KickMembers)
+                {
+                    await userAccount.KickAsync(reason);
+                    await Context.Channel.SendMessageAsync($"The user `{userAccount}` has been kicked, for {reason}");
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync("No permissions for kicking a user.");
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("This User can't be kicked, because the user has a admin role.");
+            }
+        }*/
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DiscordNukeBotv1
+ You really dont want this bot in your server
